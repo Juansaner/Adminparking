f6a4fadb92dd2302807c612f3eba97a8ded2416d
Juan Pablo <juansaner01@gmail.com>
2024-10-30 08:54:51 -0500

    Sanitización entrada de datos

--- admin/propiedades/crear_usuario.php
+++ admin/propiedades/crear_usuario.php
@@ -4,10 +4,19 @@ require "../../includes/config/database.php";
 $db = connectDatabase();
 
 //Captura los datos
-$name = $_POST['name'];
-$lastname = $_POST['lastname'];
-$email = $_POST['email'];
+$name = mysqli_real_escape_string($db, htmlspecialchars($_POST['name']));
+$lastname = mysqli_real_escape_string($db, preg_replace("/[^a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]/", "", htmlspecialchars($_POST['lastname'])));
+$email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
 $password = $_POST['password'];
+//Hashea la password
+password_hash($password, PASSWORD_DEFAULT);
+
+// Mostrar los valores sanitizados para verificar que están bien
+echo "Nombre sanitizado: " . $name . "<br>";
+echo "Apellido sanitizado: " . $lastname . "<br>";
+echo "Email sanitizado: " . $email . "<br>";
+
+exit;
 
 //Insertar base de datos
 $query = "INSERT INTO usuarios (name, lastname, email, password) VALUES ('$name', '$lastname', '$email', '$password')";
